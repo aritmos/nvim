18755937e3aba35e78bcaeed18c3cd8c157201ea
aritmos <52433836+aritmos@users.noreply.github.com>
2023-09-08 13:07:09 +0200

    add golang support

--- lua/user/config/lsp/lspconfig.lua
+++ lua/user/config/lsp/lspconfig.lua
@@ -132,3 +132,24 @@ lspconfig.zls.setup({
     capabilities = capabilities,
     on_attach = zig_on_attach,
 })
+
+
+-- Go
+local util = require "lspconfig/util"
+
+lspconfig.gopls.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = { "gopls" },
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+        gopls = {
+            completeUnimported = true,
+            usePlaceholders = true,
+            analyses = {
+                unusedparams = true,
+            }
+        }
+    }
+})
--- lua/user/config/lsp/mason.lua
+++ lua/user/config/lsp/mason.lua
@@ -20,6 +20,7 @@ mason_lspconfig.setup({
         "pyright",
         "rust_analyzer",
         "lua_ls",
+        "gopls"
     },
     -- auto-install configured servers (with lspconfig)
     -- automatic_installation = true, -- not the same as ensure_installed
