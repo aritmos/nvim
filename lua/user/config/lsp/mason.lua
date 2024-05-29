242557b0e3cb60c4090a32ee64839df892216803
aritmos <52433836+aritmos@users.noreply.github.com>
2024-05-29 22:12:18 +0200

    mason update

--- lua/user/config/lsp/mason.lua
+++ lua/user/config/lsp/mason.lua
@@ -1,20 +1,13 @@
--- import mason plugin safely
-local mason_status, mason = pcall(require, "mason")
-if not mason_status then
-    return
-end
-
--- import mason-lspconfig plugin safely
-local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not mason_lspconfig_status then
-    return
-end
-
 -- enable mason
-mason.setup()
+require("mason").setup()
 
-mason_lspconfig.setup({
-    -- list of servers for mason to install
+-- install debuggers
+require("mason-nvim-dap").setup({
+    ensure_installed = { "codelldb", "debugpy" }
+})
+
+-- install LSPs
+require("mason-lspconfig").setup({
     ensure_installed = {
         "pylsp",
         "pyright",
